ca1fedacf2950524920b57097148efde82e82e6b
PGM-LottQuas <lotte.quasters@student.arteveldehs.be>
2025-12-09 16:00:55 +0100

    oefening 2

--- week_1/oefeningen/oefeningen.js
+++ week_1/oefeningen/oefeningen.js
@@ -7,4 +7,16 @@ console.log(firstLetter);
 console.log(secondLetter);
 console.log(ThirdLetter);
 
+// Oefening 2: Destructuring met Geneste Arrays
 
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+
+const [[a], [, b], [, , c]] = matrix;
+
+console.log(a);
+console.log(b);
+console.log(c);
