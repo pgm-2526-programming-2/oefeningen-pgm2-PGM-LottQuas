186579e6de68ef8c186b0f6beadeed8efdb296da
PGM-LottQuas <lotte.quasters@student.arteveldehs.be>
2025-12-09 15:51:33 +0100

    week 3

---
+++ week_1/oefeningen/README.dm
@@ -0,0 +1,4 @@
+Oefeningen week 3
+
+https://github.com/pgm-2526-programming-2/oefeningen-pgm2-PGM-LottQuas
+
---
+++ week_1/oefeningen/oefeningen.js
@@ -0,0 +1,10 @@
+// Oefening 1: destructuring met Arrays
+
+const letters = ["A", "B", "C"];
+const [firstLetter, secondLetter, ThirdLetter] = letters;
+
+console.log(firstLetter);
+console.log(secondLetter);
+console.log(ThirdLetter);
+
+
